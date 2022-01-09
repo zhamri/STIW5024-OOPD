d3734c78fe3b753f4bfa0ba0e187781f42755206
zhamri <zhamri@gmail.com>
2022-01-09 10:27:48 +0800

    Initial commit

---
+++ src/ZhamriCheAni.java
@@ -0,0 +1,16 @@
+public class ZhamriCheAni {
+
+    public static void main(String[] zhamri) {
+        System.out.println("Hello World");
+        DisplayMyName();
+        DisplayMySonName();
+    }
+
+    public static void DisplayMyName(){
+        System.out.println("zhamri");
+    }
+
+    public static void DisplayMySonName(){
+        System.out.println("ali");
+    }
+}
