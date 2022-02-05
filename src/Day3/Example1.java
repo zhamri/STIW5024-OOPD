7ca3e965e3de9c002c582d1f2b7af5289a05bd32
zhamri <zhamri@gmail.com>
2022-02-05 11:04:17 +0800

    Add Readme.md

--- src/Day3/Example1.java
+++ src/Day3/Example1.java
@@ -1,4 +1,23 @@
 package Day3;
 
 public class Example1 {
+    public static void main(String[] args) {
+
+        int weightSimple = 5;
+        int weightAverage = 10;
+        int weightComplex = 15;
+
+        int inputUseCase1 = 0;
+        int inputUseCase2 = 29;
+        int inputUseCase3 = 0;
+
+        int simple = weightSimple * inputUseCase1;
+        int average = weightAverage * inputUseCase2;
+        int complex = weightComplex * inputUseCase3;
+
+        int total = simple + average + complex;
+
+        System.out.print("Total: " + total);
+
+    }
 }
