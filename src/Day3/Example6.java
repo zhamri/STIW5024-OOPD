ef5439a856fb9c85fe2d97006df4c96101726174
zhamri <zhamri@gmail.com>
2022-02-05 14:10:43 +0800

    Add Example6.java

---
+++ src/Day3/Example6.java
@@ -0,0 +1,10 @@
+package Day3;
+
+public class Example6 {
+
+    public static void main(String[] args) {
+        UseCaseWeight3 useCaseWeight3 = new UseCaseWeight3(0, 29, 0);
+        int total = useCaseWeight3.getTotal();
+        System.out.print("Total: " + total);
+    }
+}
